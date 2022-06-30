25007b9d2c98edcd549660d6b5f3b0d461d0c44c
EduardoVegaFlores <vegaeduardo521@gmail.com>
2022-06-29 19:46:22 -0500

    CalculadoraMMR.js
    Added arrays in calculadora

--- CalculadoraMMR.js
+++ CalculadoraMMR.js
@@ -14,52 +14,98 @@ function asignarElo(promediofinal, Division){
 }
 
 function cargar (){
-    let cantidadpartidas = parseInt(prompt("Ingrese el numero de partidas por las cuales quiera calcular el MMR"))
+    let mmrArray = []
     let mmr = 0
-    let mmrfinal = 0
-    
-    function sumammrfinal() {
-        mmrfinal = mmrfinal + mmr
-    }
-    
-    
+    let cantidadpartidas = parseInt(prompt("Ingrese el numero de partidas por las cuales quiera calcular el MMR"))
+    let promedio = 0
+
     for(let i=1; i<=cantidadpartidas; i++){
     
-    
         mmr = parseInt(prompt("Ingrese el MMR"))
-        sumammrfinal()
-    
+        mmrArray.push(mmr)
     };
-    
-    let promedio = mmrfinal/cantidadpartidas
-    let promediofinal = Math.round(promedio)
-    
 
-    
+    mmrArray.forEach(function(elemento){
+        promedio = promedio + elemento
+    })
+
+    promedio = promedio / mmrArray.length
+
+
     if (promedio>=0 && promedio<=1000){
     
-        asignarElo(promediofinal, "Bronze");
+        asignarElo(promedio, "Bronze");
     
         
     }else if (promedio>1000 && promedio<=2000){
     
-        asignarElo(promediofinal, "Plata");
+        asignarElo(promedio, "Plata");
     
     }else if (promedio>2000 && promedio<=3000){
             
-        asignarElo(promediofinal, "Oro");
+        asignarElo(promedio, "Oro");
     
     }else if (promedio>3000 && promedio<=4000){
     
-        asignarElo(promediofinal, "Platino");
+        asignarElo(promedio, "Platino");
     
     }else if (promedio>4000){
     
-        asignarElo(promediofinal, "Diamante");
+        asignarElo(promedio, "Diamante");
     
     }else {
         alert("El MMR no es valido")
     }
+
+
+
+
+
+    // let cantidadpartidas = parseInt(prompt("Ingrese el numero de partidas por las cuales quiera calcular el MMR"))
+    // let mmr = 0
+    // let mmrfinal = 0
+    
+    // function sumammrfinal() {
+    //     mmrfinal = mmrfinal + mmr
+    // }
+    
+    
+    // for(let i=1; i<=cantidadpartidas; i++){
+    
+    //     mmr = parseInt(prompt("Ingrese el MMR"))
+        
+    
+    // };
+    
+    // let promedio = mmrfinal/cantidadpartidas
+    // let promediofinal = Math.round(promedio)
+    
+
+    
+    // if (promedio>=0 && promedio<=1000){
+    
+    //     asignarElo(promediofinal, "Bronze");
+    
+        
+    // }else if (promedio>1000 && promedio<=2000){
+    
+    //     asignarElo(promediofinal, "Plata");
+    
+    // }else if (promedio>2000 && promedio<=3000){
+            
+    //     asignarElo(promediofinal, "Oro");
+    
+    // }else if (promedio>3000 && promedio<=4000){
+    
+    //     asignarElo(promediofinal, "Platino");
+    
+    // }else if (promedio>4000){
+    
+    //     asignarElo(promediofinal, "Diamante");
+    
+    // }else {
+    //     alert("El MMR no es valido")
+    // }
         
 }
 
